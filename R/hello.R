d0f10c3c9e57130db754f16d3c5c9d0453d411c0
Frojast <rsimon64@gmail.com>
2016-05-29 07:31:44 -0500

    cleanup

---
+++ R/hello.R
@@ -0,0 +1,15 @@
+
+#' hello
+#'
+#' @return string
+#' @export
+#'
+#' @examples
+#'
+#' library(fbcollect)
+#'
+#' hello()
+#'
+hello <- function(){
+  "Hi"
+}
--- inst/cache/app.R
+++ inst/cache/app.R
@@ -5,18 +5,82 @@ library("readxl")
 library(shinyBS)
 library(shinyFiles)
 library(DT)
+library(brapi)
+library(stringr)
 
 
+dburl = "sweetpotatobase-test.sgn.cornell.edu"
+locos = Sys.getenv("OS")
+
+if(str_detect(locos, "Windows")){
+  hddir = file.path(Sys.getenv("LOCALAPPDATA"), "HIDAP")
+} else {
+  hddir = file.path("usr", "HIDAP")
+}
+  if(!dir.exists(hddir)) dir.create(hddir, recursive = TRUE)
+
+
+login <- yaml::yaml.load_file("login.yaml")
+
+session = NULL
+fileLocs = file.path(hddir, "locs.rds")
+
+cacheLocationData <- function(){
+  pb <- progress::progress_bar$new(total = 1e7, clear = FALSE, width = 60,
+                                   format = "  downloading :what [:bar] :percent eta: :eta")
+  pb$tick(1, tokens = list(what = "connect to db"))
+
+  if(can_internet(dburl)){
+    pb$tick(1e7/2, tokens = list(what = "retrieve data"))
+    brapi = brapi_con("sweetpotato",
+                      paste0(login$sgn, dburl ),
+                      80, login$user, login$login)
+    locs = brapi::locations_list()
+    if(nrow(locs) > 0){
+      pb$tick(1e7, tokens = list(what = "cache data"))
+      saveRDS(locs, file = fileLocs)
+      #pb$tick(1e7, tokens = list(what = "finished data download"))
+    }
+  }
+}
+
+cacheLocationData()
+
+locsData <- reactiveFileReader(1000, session, filePath = fileLocs, readRDS)
 
 ui <- dashboardPage(skin = "yellow",
                     dashboardHeader(title = "HIDAP collect"),
                     dashboardSidebar(disable = TRUE),
                     dashboardBody(
                       shinyjs::useShinyjs(),
-                      tabBox(width = 12, selected = "Config", height = 500,
-                             tabPanel("Config",
+                      tabBox(width = 12, selected = "Data source", height = 500,
+                             tabPanel("Data source",
+                              fluidRow(
+                                column(4,
+                                  radioButtons("crop", "Crop",
+                                               c("potato", "sweetpotato", "cassava",
+                                                         "yam", "musa"), "sweetpotato",
+                                               inline = TRUE
+                                               ),
+                                  radioButtons("sourceType", "Data source type",
+                                               choices = c("Local",
+                                                           "BrAPI",
+                                                           "CloneSelector", "DataCollector",
+                                                           "AccuDataLog", "Fieldbook App"),
+                                               selected = "Local", inline = TRUE)
 
-                                uiOutput("cache_test")
+                                       ),
+
+                                column(8,
+                                  tabBox(width = 12,
+                                    tabPanel("Locations",
+                                             DT::dataTableOutput("cache_test")
+                                             )
+                                  )
+
+
+                                )
+                              )
 
                              )
                       )
@@ -27,9 +91,20 @@ ui <- dashboardPage(skin = "yellow",
 
 server <- function(input, output, session) {
 
-  output$cache_test <- renderText({
-    "Hello, world!"
+  sharedValues <- reactiveValues()
+
+  observe({
+    if(file.exists(fileLocs)){
+      sharedValues[['data']] <- locsData()
+    } else {
+      cacheLocationData()
+    }
+
   })
+
+  output$cache_test <- DT::renderDataTable({
+    sharedValues[['data']]
+  }, options = list(scrollX = TRUE))
 
 }
 
